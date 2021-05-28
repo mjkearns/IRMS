7b15d3d05228dcbc00ac71a8177a8adfb74d1e78
skerrats <skerr@ats-t1.com>
2021-05-28 12:58:21 +1000

    EA 54 - enable rabbitmq shared package

---
+++ packages/shared-package-test/index.js
@@ -0,0 +1,51 @@
+const MessageSystem = require('@ats-irms/message-system-rabbitmq')
+
+async function run() {
+  const host = 'localhost:25672'
+
+  // instantiate and connect (also creates an initial pipe/channel on the connection)
+  console.log('Connecting to ' + host)
+  const system = await new MessageSystem(host)
+
+  const pipeId = null // here null means the first available pipe
+
+  // alternativly you can do this in two stages
+  // let system = await new MessageSystem()
+  // let pipeId = system.connect(host)
+
+  // if you require mulitple pipes/channels on the connections
+  // let pipeId2 = system.newPipe()
+
+  if (!system.connected()) {
+    console.log('Failed to connect - is RabbitMQ running?')
+    return
+  }
+
+  console.log('Subscribing to simple.example.#')
+  system.subscribe(pipeId, 'example', 'simple.example.#', handleMessage)
+
+  const timer1 = setInterval(() => {
+    const message = 'Hello ' + Math.round(Math.random() * 1000)
+    system.publish(pipeId, 'example', 'simple.example.hello', message)
+    console.log('Published: ' + message)
+  }, 1500)
+
+  const timer2 = setInterval(() => {
+    const message = 'Goodbye ' + Math.round(Math.random() * 1000)
+    system.publish(pipeId, 'example', 'simple.example.goodbye', message)
+    console.log('Published: ' + message)
+  }, 2000)
+
+  setTimeout(() => {
+    console.log('Shutting down')
+    clearInterval(timer1)
+    clearInterval(timer2)
+    system.disconnect()
+  }, 10000)
+}
+
+function handleMessage(message, fields, properties) {
+  console.log('Received: ' + message)
+}
+
+run()
