44f8a8983223fbf7a41486c40409a01a64f1ae22
mwrennall <mwrennall@atspl.com.au>
2021-06-02 14:36:24 +1000

    EA-57 added request-response pattern to message queue
    - added fetch and listen methods to message system
    - improved unit test coverage
    - created a fetch example

--- packages/message-system-rabbitmq/message-system-rabbitmq.test.js
+++ packages/message-system-rabbitmq/message-system-rabbitmq.test.js
@@ -10,7 +10,8 @@ const example = {
     api: '1.1.0',
     message: { header: 'Welcome', body: 'Hello World!' }
   },
-  messageString: 'Unit testing for the win!'
+  messageString: 'Unit testing for the win!',
+  messageNumber: 1234
 }
 
 describe('connections', () => {
@@ -40,14 +41,25 @@ describe('connections', () => {
     expect(system.lastError).not.toBeNull()
   })
 
-  it('connect sucessfully', async () => {
+  it('check connected when not', async () => {
+    const connected = system.connected()
+    expect(connected).toBe(false)
+  })
+
+  it('connect successfully', async () => {
     const pipeId = await system.connect(host)
     expect(pipeId).not.toBe(false)
     expect(pipeId).toBeGreaterThan(0)
     expect(system.lastError).toBeNull()
   })
 
-  it('disconnect sucessfully', async () => {
+  it('check connected when connected', async () => {
+    await system.connect(host)
+    const connected = system.connected()
+    expect(connected).toBe(true)
+  })
+
+  it('disconnect successfully', async () => {
     await system.connect(host)
     const disconnect = await system.disconnect()
     expect(disconnect).toBe(true)
@@ -89,7 +101,7 @@ describe('producers', () => {
     expect(ok).toBe(false)
   })
 
-  it('publish sucessfully', async () => {
+  it('publish successfully', async () => {
     const pipeId = await system.connect(host)
     const ok = await system.publish(
       pipeId,
@@ -207,5 +219,113 @@ describe('consumers', () => {
       example.messageString
     )
     expect(pubOk).toBe(true)
+  })
+
+  it('subscribe and receive number', async (done) => {
+    const pipeId = await system.connect(host)
+    const subOk = await system.subscribe(
+      pipeId,
+      example.queue,
+      example.topic,
+      (message, fields, properties) => {
+        expect(message).toBe(example.messageNumber)
+        expect(fields).toBeDefined()
+        expect(properties).toBeDefined()
+        expect(properties.contentType).toBe('text/plain')
+        done()
+      }
+    )
+    expect(subOk).toBe(true)
+
+    const pubOk = await system.publish(
+      pipeId,
+      example.queue,
+      example.topic,
+      example.messageNumber
+    )
+    expect(pubOk).toBe(true)
+  })
+})
+
+describe('prosumers', () => {
+  beforeEach(async () => {
+    system = await new MessageSystem()
+  })
+
+  afterEach(async () => {
+    await system.disconnect()
+  })
+
+  it('fetch no response timeout', async () => {
+    const pipeId = await system.connect(host)
+    const ok = await system.fetch(
+      pipeId,
+      example.queue,
+      example.messageString,
+      { timeout: 2000 }
+    )
+    expect(ok).toBe(false)
+  })
+
+  it('fetch invalid pipe', async () => {
+    const ok = await system.fetch(
+      999999,
+      example.queue,
+      example.messageString,
+      { timeout: 2000 }
+    )
+    expect(ok).toBe(false)
+  })
+
+  it('listen', async (done) => {
+    const pipeId = await system.connect(host)
+    const listenOk = await system.listen(
+      pipeId,
+      'abc',
+      (message, fields, properties) => {
+        // never fires
+        done.fail('unexpected response')
+      }
+    )
+    expect(listenOk).toBe(true)
+    done()
+  })
+
+  it('listen invalid pipe', async (done) => {
+    const listenOk = await system.listen(
+      999999,
+      'abc',
+      (message, fields, properties) => {
+        // never fires
+        done.fail('unexpected response')
+      }
+    )
+    expect(listenOk).toBe(false)
+    done()
+  })
+
+  it('fetch with response', async () => {
+    const pipeId = await system.connect(host)
+    const listenOk = await system.listen(
+      pipeId,
+      'abc',
+      (message, fields, properties) => {
+        expect(message).toEqual(example.messageString)
+        return 'Fetched Data'
+      }
+    )
+    expect(listenOk).toBe(true)
+
+    const { content, fields, properties } = await system.fetch(
+      pipeId,
+      'abc',
+      example.messageString,
+      {
+        timeout: 2000
+      }
+    )
+    expect(fields).toBeDefined()
+    expect(properties.contentType).toBe('text/plain')
+    expect(content).toBe('Fetched Data')
   })
 })
--- packages/message-system-rabbitmq/unit-test.sh
+++ packages/message-system-rabbitmq/unit-test.sh
@@ -2,14 +2,14 @@ echo "Launching Containers"
 docker start rabbitmq-unit-test || docker run -d --name rabbitmq-unit-test -p 25672:5672 rabbitmq
 
 echo -n "Waiting for Containers: "
-until [ "`curl -s -X GET http://localhost:25672/api`" ]; do
-    echo -n .
-    sleep 0.2;
-done;
+until [ "$(curl -s -X GET http://localhost:25672/api)" ]; do
+  echo -n .
+  sleep 0.2
+done
 echo " Ready"
 
 echo "Launching Test Runner"
-jest --watchAll
+jest --watch --coverage
 
 echo "Cleanup"
 docker stop rabbitmq-unit-test
